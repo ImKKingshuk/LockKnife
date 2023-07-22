de60079726639cc5ef1cbd6895f0d07039244c6d
ImKKingshuk <hello@kingshukmondal.com>
2023-07-23 04:52:02 +0530

    Updated LockKnife v1.1

--- LockKnife.sh
+++ LockKnife.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 function print_banner() {
     echo "******************************************"
     echo "*                LockKnife               *"
@@ -12,25 +11,20 @@ function print_banner() {
     echo
 }
 
-
 function recover_android_password() {
-   
+    local device_serial
+
     read -p "Enter your Android device serial number: " device_serial
 
-   
-   
     adb connect "$device_serial"
 
- 
     devices_output=$(adb devices)
     if [[ ! $devices_output =~ $device_serial ]]; then
         echo "Failed to connect to the device with serial number: $device_serial"
         return
     fi
 
-  
     adb -s "$device_serial" pull /data/system/gesture.key
-
 
     password=""
     while IFS= read -r -n1 byte; do
@@ -41,27 +35,28 @@ function recover_android_password() {
 
     echo "Recovered password: $password"
 
-  
     rm gesture.key
 }
-
 
 function execute_lockknife() {
     print_banner
 
-  
     if ! command -v adb &>/dev/null; then
         echo "Error: ADB (Android Debug Bridge) not found. Please install ADB and make sure it's in your PATH."
+        echo "You can download ADB from the Android SDK platform-tools. Follow the instructions for your OS:"
+        echo "Windows: https://developer.android.com/studio/#command-tools"
+        echo "MacOS: https://developer.android.com/studio/#downloads"
+        echo "Linux: https://developer.android.com/studio/#downloads"
         exit 1
     fi
 
     recover_android_password
 }
 
-
+# Automatically execute the script only if it's not being sourced
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
-   
+    # Set execute permission for the script
     chmod +x "$0"
-   
+    # Execute LockKnife
     execute_lockknife
 fi
