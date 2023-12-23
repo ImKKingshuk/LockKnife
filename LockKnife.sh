f3914710e41260cfc3205c1f49bfaeff6e6aa48f
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-12-23 20:41:47 +0530

    updated LockKnife v1.2.0

--- LockKnife.sh
+++ LockKnife.sh
@@ -3,7 +3,7 @@
 function print_banner() {
     echo "******************************************"
     echo "*                LockKnife               *"
-    echo "*    Android LockScreen Password Tool    *"
+    echo "*         Android Password Tool          *"
     echo "*      ----------------------------      *"
     echo "*                        by @ImKKingshuk *"
     echo "* Github- https://github.com/ImKKingshuk *"
@@ -13,6 +13,7 @@ function print_banner() {
 
 function recover_android_password() {
     local device_serial
+    local recovery_option
 
     read -p "Enter your Android device serial number: " device_serial
 
@@ -24,18 +25,67 @@ function recover_android_password() {
         return
     fi
 
-    adb -s "$device_serial" pull /data/system/gesture.key
+    echo "Select recovery option:"
+    echo "1. Gesture Lock"
+    echo "2. Password Lock"
+    echo "3. Wi-Fi Passwords"
+    read -p "Enter your choice (1/2/3): " recovery_option
 
-    password=""
+    case $recovery_option in
+        1)
+            adb -s "$device_serial" pull /data/system/gesture.key
+            recover_password "gesture.key" ;;
+        2)
+            adb -s "$device_serial" pull /data/system/password.key
+            recover_password "password.key" ;;
+        3)
+            recover_wifi_passwords ;;
+        *)
+            echo "Invalid choice. Exiting."
+            ;;
+    esac
+}
+
+function recover_password() {
+    local file_path="$1"
+    local password=""
+    
     while IFS= read -r -n1 byte; do
         byte_value=$(printf "%d" "'$byte")
         decrypted_byte=$((byte_value ^ 0x6A))
         password+=$(printf "\\$(printf '%03o' "$decrypted_byte")")
-    done < gesture.key
+    done < "$file_path"
 
     echo "Recovered password: $password"
 
-    rm gesture.key
+    rm "$file_path"
+}
+
+function recover_wifi_passwords() {
+    local wifi_file="/data/misc/wifi/wpa_supplicant.conf"
+    local device_serial
+    local password=""
+
+    read -p "Enter your Android device serial number: " device_serial
+
+    adb connect "$device_serial"
+
+    devices_output=$(adb devices)
+    if [[ ! $devices_output =~ $device_serial ]]; then
+        echo "Failed to connect to the device with serial number: $device_serial"
+        return
+    fi
+
+    adb -s "$device_serial" pull "$wifi_file"
+    
+    while IFS= read -r line; do
+        if [[ $line =~ psk=\"(.*)\" ]]; then
+            password="${BASH_REMATCH[1]}"
+            echo "Recovered Wi-Fi password: $password"
+        fi
+    done < "$wifi_file"
+
+    rm "$wifi_file"
 }
 
 function execute_lockknife() {
@@ -53,10 +103,10 @@ function execute_lockknife() {
     recover_android_password
 }
 
-# Automatically execute the script only if it's not being sourced
+
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
-    # Set execute permission for the script
+   
     chmod +x "$0"
-    # Execute LockKnife
+ 
     execute_lockknife
 fi
